00b5fa9da1011029e203a3b941cedae2c1e26d66
Matteo Parrucci <parruc@gmail.com>
2013-06-22 19:30:30 +0200

    better views for bagni

--- ombrelloni/bagni/views.py
+++ ombrelloni/bagni/views.py
@@ -1,1 +1,28 @@
-# Create your views here.
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from models import Bagno, Service
+
+class BagniView(ListView):
+
+    model = Bagno
+
+    def get_context_data(self, **kwargs):
+        context = super(BagniView, self).get_context_data(**kwargs)
+        return context
+
+class BagnoView(DetailView):
+
+    model = Bagno
+
+    def get_context_data(self, **kwargs):
+        context = super(BagnoView, self).get_context_data(**kwargs)
+        return context
+
+class ServiceView(DetailView):
+
+    model = Service
+
+    def get_context_data(self, **kwargs):
+        context = super(ServiceView, self).get_context_data(**kwargs)
+        return context
