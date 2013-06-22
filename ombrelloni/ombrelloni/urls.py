93e42d43d37b6788f2fd1c0334964eea3bdb4a17
Matteo Parrucci <parruc@gmail.com>
2013-06-22 19:31:06 +0200

    installed south

--- ombrelloni/ombrelloni/urls.py
+++ ombrelloni/ombrelloni/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import ugettext_lazy as _
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib.gis import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ombrelloni.views.home', name='home'),
-    # url(r'^ombrelloni/', include('ombrelloni.foo.urls')),
+urlpatterns = i18n_patterns('',
+    #Bagni urls
+    url(_(r'^bagni/'), include('bagni.urls')),
+)
+
+urlpatterns += patterns(
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
