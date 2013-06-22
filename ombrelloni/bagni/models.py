6d47e3bb6afc1c6c10c055d54b56f3e15ff85fc3
Matteo Parrucci <parruc@gmail.com>
2013-06-22 19:29:21 +0200

    better model for bagni

--- ombrelloni/bagni/models.py
+++ ombrelloni/bagni/models.py
@@ -1,12 +1,67 @@
 from django.contrib.gis.db import models
+from django.utils.translation import ugettext_lazy as _
+import autoslug
 
 # Create your models here.
 
 class Bagno(models.Model):
+    class Meta:
+        verbose_name = _('Bagno')
+        verbose_name_plural = _('Bagni')
+
     name = models.CharField(max_length=50)
+    slug = autoslug.AutoSlugField(max_length=50,
+                                  populate_from='name',
+                                  verbose_name=_("Slug"))
+    number = models.CharField(max_length=10, blank=True)
+    services = models.ManyToManyField("Service", blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    mail = models.CharField(max_length=50, blank=True)
+    tel = models.CharField(max_length=50, blank=True)
+    fax = models.CharField(max_length=50, blank=True)
+    site = models.URLField(max_length=50, blank=True)
     point = models.PointField()
 
     objects = models.GeoManager()
 
     def __unicode__(self):
         return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("bagno", [self.slug, ])
+
+class Service(models.Model):
+    class Meta:
+        verbose_name = _('Service')
+        verbose_name_plural = _('Services')
+
+    BEACH = "BE"
+    SPORT = "SP"
+    RENT = "RE"
+    OTHER = "OT"
+    SERVICE_CATEGORIES = (
+        (BEACH, _('Beach')),
+        (SPORT, _('Sport')),
+        (RENT, _('Rent')),
+        (OTHER, _('Other')),
+    )
+
+    name = models.CharField(max_length=50)
+    slug = autoslug.AutoSlugField(max_length=50,
+                                  populate_from='name',
+                                  verbose_name=_("Slug"))
+    category = models.CharField(max_length=50,
+                                blank=True,
+                                choices=SERVICE_CATEGORIES,
+                                default=OTHER)
+    free = models.BooleanField(default=True)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("service", [self.slug, ])
+
+    def __unicode__(self):
+        return self.name
+
