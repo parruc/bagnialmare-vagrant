2633b8360b310913a2d683339c6865c2f07ec2c9
Matteo Parrucci <parruc@gmail.com>
2013-06-25 02:52:19 +0200

    added dev and local DBs

--- ombrelloni/ombrelloni/settings.py
+++ ombrelloni/ombrelloni/settings.py
@@ -10,7 +10,16 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
+    'dev': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ombrelloni',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'ombrelloni',
+        'PASSWORD': 'ombrelloni',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    },
+    'local': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ombrelloni',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
