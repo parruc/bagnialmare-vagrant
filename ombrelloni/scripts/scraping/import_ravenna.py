3bf8be5ee540002656ec10dc8544b0abb91caa97
Matteo Parrucci <parruc@gmail.com>
2013-06-26 23:19:12 +0200

    fixed error in ravenna import

--- ombrelloni/scripts/scraping/import_ravenna.py
+++ ombrelloni/scripts/scraping/import_ravenna.py
@@ -13,16 +13,19 @@ localita = []
 bagni = []
 parsed_page = utils.try_open_file_or_url(url=URL, name="ravenna", count=1)
 urls_localita = parsed_page.xpath("//div[contains(@class, 'wrapper') and contains(@class, 'intermedia')]//ul/li/a/@href")
+j = 0
 for i, url_localita in enumerate(urls_localita[:-2], start=1):
     parsed_localita = utils.try_open_file_or_url(url=BASE_URL + url_localita, name="ravenna_localita", count=i)
     localita = parsed_localita
     urls_bagni = parsed_localita.xpath("//div[contains(@class, 'wrapper') and contains(@class, 'intermedia')]//ul/li/a/@href")
-    for j, url_bagno in enumerate(urls_bagni, start=1):
+    for url_bagno in urls_bagni:
+        j += 1
         print "importing %s" % url_bagno
         if url_bagno in urls_localita:
             continue
         bagno = {}
         parsed_bagno = utils.try_open_file_or_url(url=BASE_URL + url_bagno, name="ravenna_bagno", count=j)
+
         bagno['name'] = parsed_bagno.xpath("//h2[@id='contenutoPagina']/text()")[0].strip()
         available_names = {"indirizzo":"address", "telefono": "tel" , "telefax": "fax", "email": "mail", "link": "site", "sito web": "site"}
         recapito = parsed_bagno.xpath("//div[@class='sectionRight']//dl[@class='recapito']")[0]
