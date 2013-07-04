2541ccba1e4879224af97f4cd28c20378e3943d6
Matteo Parrucci <parruc@gmail.com>
2013-07-04 19:26:17 +0200

    better advanced search faceting

--- ombrelloni/bagni/search_indexes.py
+++ ombrelloni/bagni/search_indexes.py
@@ -5,18 +5,21 @@ from bagni.models import Bagno
 class BagnoIndex(indexes.SearchIndex, indexes.Indexable):
     name = indexes.CharField(model_attr='name')
     city = indexes.CharField(model_attr='city')
+    #TODO: Facets not working here... Study why
+    #city = indexes.FacetCharField(model_attr='city')
     text = indexes.EdgeNgramField(document=True, use_template=True)
 
     services = indexes.FacetMultiValueField()
     # Other field definitions
 
-
-    def prepare(self, obj):
-        data = super(BagnoIndex, self).prepare(obj)
-        data['services'] = list()
+    def prepare_services(self, obj):
+        values = list()
         for service in obj.services.all():
-            data['services'].append(service.name)
-        return data
+            values.append(service.name)
+        return values
+
+    # def prepare_city(self, obj):
+    #     return obj.city
 
     def get_model(self):
         return Bagno
