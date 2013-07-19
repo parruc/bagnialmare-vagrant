98a6142070cec7fc2aabd4707efee16d8249de69
Marco Bartolini <marco.bartolini@gmail.com>
2013-07-19 12:22:16 +0200

    minor changes and osx compatibility

--- prepare_development.sh
+++ prepare_development.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-sshfs vagrant-ombrelloni:/var/www/ombrelloni.it /home/creepingdeath/django/4hm -o uid=$(id -u) -o gid=$(id -g)
-cd /home/creepingdeath/django/4hm/django
-git config --global user.email "parruc@gmail.com"
-git config --global user.name "Matteo Parrucci"
+GIT_USER=flyingfrog
+VAGRANT_DIR=$HOME/Projects/4hm/vm
+REPO_DIR=$VAGRANT_DIR/django
+echo "repo dir: $REPO_DIR"
+sshfs vagrant-ombrelloni:/var/www/ombrelloni.it $VAGRANT_DIR -o uid=$(id -u) -o gid=$(id -g) -o defer_permissions
+cd $REPO_DIR
+#git config --global user.email "parruc@gmail.com"
+#git config --global user.name "Matteo Parrucci"
 git remote rm origin
-git remote add origin https://parruc@bitbucket.org/flyingfrog/4hm.git
+git remote add origin https://$GIT_USER@bitbucket.org/flyingfrog/4hm.git
 
