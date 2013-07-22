df07e96290b1c1d688f285f6b4504ba79492df11
Matteo Parrucci <parruc@gmail.com>
2013-07-22 13:39:00 +0200

    added city view, homepage and styled a bit the search page

--- bagni/views.py
+++ bagni/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView
@@ -6,6 +7,7 @@ from django.utils.translation import ugettext as _
 
 from models import Bagno, Service
 from search import search
+
 
 class BagniView(ListView):
     """View for a list of bagni
@@ -16,6 +18,7 @@ class BagniView(ListView):
         context = super(BagniView, self).get_context_data(**kwargs)
         return context
 
+
 class BagnoView(DetailView):
     """Detail view for a single bagno
     """
@@ -24,6 +27,7 @@ class BagnoView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(BagnoView, self).get_context_data(**kwargs)
         return context
+
 
 class ServiceView(DetailView):
     """Detail view for a single service
@@ -35,10 +39,40 @@ class ServiceView(DetailView):
         return context
 
 
-class SearchView(TemplateView):
+class CityView(ListView):
+    """Detail view for a single city
     """
-    Simple search view, which accepts search queries via url, like google.
-    Use something like ?q=this+is+the+serch+term
+    template_name = "bagni/city.html"
+    model = Bagno
+    allow_empty = False
+
+    def get_queryset(self):
+        try:
+            city = self.kwargs['city']
+        except KeyError:
+            raise Http404(_("No city specified"))
+        return self.model.objects.filter(city=city)
+
+    def get_context_data(self, **kwargs):
+        context = super(CityView, self).get_context_data(**kwargs)
+        return context.extend({'city': city})
+
+
+class HomepageView(TemplateView):
+    """Homepage view with search form that points to the SearchView
+    """
+
+    template_name = "bagni/homepage.html"
+
+    def get_context_data(self, **kwargs):
+        pass
+
+
+class SearchView(TemplateView):
+    """Simple search view, which accepts search queries via url, like google.
+    accepts 2 params:
+     * q is the full text query
+     * f is the list of active filters narrowing the search
     """
 
     template_name = "bagni/search.html"
@@ -51,7 +85,7 @@ class SearchView(TemplateView):
         new_query_string = self.request.GET.copy()
         if q == '':
             messages.add_message(self.request, messages.WARNING,
-                             _("Inserisci del testo nel box di ricerca"))
+                                 _("Inserisci del testo nel box di ricerca"))
             return {}
         if q is not None:
             query = q.replace('+', ' AND ').replace(' -', ' NOT ')
--- ombrelloni/urls.py
+++ ombrelloni/urls.py
@@ -1,21 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
-from django.utils.translation import ugettext_lazy as _
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib.gis import admin
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
-    #Bagni urls
-    url(_(r'^bagni/'), include('bagni.urls')),
-)
-
-urlpatterns += patterns(
-
+urlpatterns = patterns(
+    '',
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += i18n_patterns(
+    '',
+    # Bagni urls
+    url(r'^', include('bagni.urls')),
 )
