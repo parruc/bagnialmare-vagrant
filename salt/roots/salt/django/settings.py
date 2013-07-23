0d1ad1e65d3aa5d432a6b1bb11d149560f4a4087
root <root@dev.creepingserver.it>
2013-07-23 08:50:23 -0400

    added developer toolbar

--- salt/roots/salt/django/settings.py
+++ salt/roots/salt/django/settings.py
@@ -8,7 +8,9 @@ TEMPLATE_DEBUG = {{ django.debug }}
 {% if django.debug %}
 def show_toolbar(request):
     return True
-SHOW_TOOLBAR_CALLBACK = show_toolbar
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
 {% endif %}
 
 ADMINS = (
