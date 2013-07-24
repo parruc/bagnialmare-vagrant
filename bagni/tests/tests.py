becc4ee214b8eb68db5cbcd6c063ae98799fd4b2
Marco Bartolini <marco.bartolini@gmail.com>
2013-07-24 14:58:44 +0200

    UnitTest infrastructure and trivial tests

---
+++ bagni/tests/tests.py
@@ -0,0 +1,73 @@
+from django.test import TestCase
+from django.contrib.gis.geos import Point
+from bagni.models import Bagno
+
+def create_dummy_bagno(name="bagno test"):
+    b = Bagno.objects.create(
+        name = name,
+        number = "18",
+        address = "via test n. 0",
+        city = "TestCity",
+        mail = "mail@example.com",
+        point = Point([40.0, 50.0]),
+        )
+    b.save()
+    return b
+
+
+class BagnoModelTest(TestCase):
+    """
+    What should we test in the models? 
+    """
+    def setUp(self):
+        """
+        This already tests wether we can create a new object or not
+        """
+        self.added = create_dummy_bagno()
+
+    def test_basic_retrival(self):
+        """
+        Does this test make any sense?
+        """
+        self.assertTrue(self.added in Bagno.objects.all())
+
+    def test_index_features(self):
+        """
+        this test should definitely exist as it tests a method we added
+        extending the basic django features.
+        What should we test though?! If this test is a bare replication of the
+        method defined in the model it makes no sense at all ...
+        """
+        b = Bagno.objects.all()[0]
+        self.assertTrue(hasattr(b, 'index_features'))
+        features = b.index_features()
+        self.assertEqual(features['id'], unicode(b.id))
+        self.assertTrue(features.has_key('city'))
+        self.assertTrue(features.has_key('text'))
+        self.assertTrue(features.has_key('services'))
+
+
+class HomePage(TestCase):
+    """
+    Very very simple test case. Just shows how to use the client.get method
+    """
+    def test_home_page_template(self):
+        self.assertTrue(len(Bagno.objects.all()) == 0) #true if no fixture
+        response = self.client.get("/it/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], "bagni/homepage.html")
+
+
+class BagniPage(TestCase):
+    """
+    Let's begin to create stuff and test the database functionalities and view
+    argument passing
+    """
+
+    fixtures = ['bagni.json']
+
+    def test_view_params(self):
+        self.assertTrue(len(Bagno.objects.all()) > 10) #false if fixtures
+        response = self.client.get('/it/bagni/')
+        self.assertIn('object_list', response.context)
+
