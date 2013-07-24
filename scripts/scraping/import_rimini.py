aad874a63cf52f5d2a57a6a5b11fe139015d530a
Matteo Parrucci <parruc@gmail.com>
2013-07-24 12:00:57 +0200

    finished parsing of rimini

--- scripts/scraping/import_rimini.py
+++ scripts/scraping/import_rimini.py
@@ -11,6 +11,7 @@ DETAILS = utils.read_details()
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
 bagni = []
 name_and_number_from_title = re.compile("((Bagno ([0-9\-/A-D]*) (sud|nord)?[ -]*(.+)?)|(.+))")
+replace_number_insensitive = re.compile("per clienti", re.IGNORECASE)
 # arernile_demaniale = re.compile(" - Arenile demaniale [0-9]+")
 parsed_page = utils.try_open_file_or_url(url=URL, name="rimini", count=1)
 trs = parsed_page.xpath("//table/tbody/tr")
@@ -44,12 +45,17 @@ for i, tr in enumerate(trs, start=1):
     bagno['details'] = {}
     for bagno_detail in bagno_details:
         titoletto = bagno_detail.xpath("./span[@class='titoletto']")
-        if len(titoletto) > 0:
+        if len(titoletto) > 0 and titoletto[0].text_content().strip() != "":
             titoletto = titoletto[0]
             titoletto_text = titoletto.text_content().strip()
-            detail_text = bagno_detail.text_content().replace(titoletto_text, "", 1).replace(":", "")
-            if titoletto_text == u"Telefono":
-                bagno['tel'] = detail_text
+            detail_text = bagno_detail.text_content().replace(titoletto_text, "", 1).replace(":", "").strip("\r\n\t -").lower()
+            if titoletto_text in (u"Telefono", u"Recapito periodo invernale"):
+                if titoletto_text == u"Recapito periodo invernale":
+                    detail_text += "(recapito invernale)"
+                if 'tel' in bagno:
+                    bagno['tel'] += detail_text
+                else:
+                    bagno['tel'] = ""
             elif titoletto_text == u"Indirizzo":
                 bagno['address'] = detail_text
             elif titoletto_text == u"Fax":
@@ -61,33 +67,46 @@ for i, tr in enumerate(trs, start=1):
             elif titoletto_text == u"www":
                 bagno['site'] = bagno_detail.xpath("./a/@href")[0]
             elif titoletto_text == u"Numero Ombrelloni":
-                detail_num = detail_text.split("/")[0].replace("Per clienti", "").strip(" -")
+                detail_num = replace_number_insensitive.sub("", detail_text.split("/")[0]).strip("\t\n\r -")
                 bagno['details']['ombrelloni'] = int(detail_num)
             elif titoletto_text == u"Numero Cabine":
-                detail_num = detail_text.split("/")[0].replace("Per clienti", "").strip(" -")
+                detail_num = replace_number_insensitive.sub("", detail_text.split("/")[0]).strip("\t\n\r -")
                 bagno['details']['cabine'] = int(detail_num)
             elif titoletto_text == u"Servizi offerti":
-                service_list = utils.get_service_from_alias(detail_text)
+                if 'description' in bagno:
+                    bagno['description'] += " " + titoletto_text + " " + detail_text
+                else:
+                    bagno['description'] = titoletto_text + " " + detail_text
+            elif titoletto_text == u"Ristorazione":
+                if "ristorazione" not in bagno['services']:
+                    bagno['services'].append("Ristorazionezione")
+            elif titoletto_text in ["Orario feriale", "Orario festivo", "Data ultimo aggiornamento", "Come arrivare", "Periodo di apertura", "Chiusura settimanale"]:
+                pass
+            else:
+                if 'description' in bagno:
+                    bagno['description'] += " " + titoletto_text + " " + detail_text
+                else:
+                    bagno['description'] = titoletto_text + " " + detail_text
+        else:
+            service_name = bagno_detail.text_content().strip("\n\r\t -")
+            if service_name.lower() in ["non accettano carte di credito", "non si accettano carte di credito"]:
+                pass
+            elif service_name in ("Acceso ai cani", ):
+                service_list = utils.get_service_from_alias(service_name.lower())
                 for service in service_list:
                     if service:
                         if not service in SERVICES:
                             SERVICES.append(service)
                         if not service in bagno['services']:
                             bagno['services'].append(service)
-            elif titoletto_text == u"Ristorazione":
-                if "ristorazione" not in bagno['services']:
-                    bagno['services'].append("Ristorazionezione")
-            elif titoletto_text in ["Orario feriale", "Orario festivo", "Data ultimo aggiornamento", "Come arrivare", "Periodo di apertura", "Chiusura settimanale"]:
-                pass
-            elif detail_text.strip().replace("-", "").strip() in ["Non accettano carte di credito", "Non si accettano carte di credito"]:
-                pass
             else:
-                print "WARNING titoletto '%s' with text '%s' for bagno '%s'" % (titoletto_text, detail_text, bagno['name'])
-        else:
-            print "ERROR titoletto '%s' with text '%s' for bagno '%s'" % (titoletto_text, detail_text, bagno['name'])
+                if 'description' in bagno:
+                    bagno['description'] += " " + service_name
+                else:
+                    bagno['description'] = service_name
     bagno_services = sviluppo.xpath("./fieldset/div[@class='linea1']|/div[@class='linea2']")
     for bagno_service in bagno_services:
-        service_list = utils.get_service_from_alias(bagno_service.text_content().strip())
+        service_list = utils.get_service_from_alias(bagno_service.text_content().strip().lower())
         for service in service_list:
             if service:
                 if not service in SERVICES:
@@ -95,11 +114,11 @@ for i, tr in enumerate(trs, start=1):
                 if not service in bagno['services']:
                     bagno['services'].append(service)
 
-
     bagni.append(bagno)
+
 
 utils.write_services(SERVICES)
 utils.write_details(DETAILS)
 
 with open('output_rimini.json', 'w') as outfile:
-  simplejson.dump(bagni, outfile, sort_keys=True, indent=4,)
+    simplejson.dump(bagni, outfile, sort_keys=True, indent=4,)
