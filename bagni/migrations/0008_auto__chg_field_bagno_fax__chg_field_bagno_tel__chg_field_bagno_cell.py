c22533832de6e1b73adcbf767fc21d8e8ec4d6eb
Matteo Parrucci <parruc@gmail.com>
2013-07-25 13:59:43 +0200

    migration for data expansion

---
+++ bagni/migrations/0008_auto__chg_field_bagno_fax__chg_field_bagno_tel__chg_field_bagno_cell.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Bagno.fax'
+        db.alter_column(u'bagni_bagno', 'fax', self.gf('django.db.models.fields.CharField')(max_length=125))
+
+        # Changing field 'Bagno.tel'
+        db.alter_column(u'bagni_bagno', 'tel', self.gf('django.db.models.fields.CharField')(max_length=125))
+
+        # Changing field 'Bagno.cell'
+        db.alter_column(u'bagni_bagno', 'cell', self.gf('django.db.models.fields.CharField')(max_length=125))
+
+    def backwards(self, orm):
+
+        # Changing field 'Bagno.fax'
+        db.alter_column(u'bagni_bagno', 'fax', self.gf('django.db.models.fields.CharField')(max_length=100))
+
+        # Changing field 'Bagno.tel'
+        db.alter_column(u'bagni_bagno', 'tel', self.gf('django.db.models.fields.CharField')(max_length=100))
+
+        # Changing field 'Bagno.cell'
+        db.alter_column(u'bagni_bagno', 'cell', self.gf('django.db.models.fields.CharField')(max_length=100))
+
+    models = {
+        u'bagni.bagno': {
+            'Meta': {'object_name': 'Bagno'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'cell': ('django.db.models.fields.CharField', [], {'max_length': '125', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '500'}),
+            'fax': ('django.db.models.fields.CharField', [], {'max_length': '125', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mail': ('django.db.models.fields.EmailField', [], {'max_length': '50', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            'point': ('django.contrib.gis.db.models.fields.PointField', [], {'null': 'True', 'blank': 'True'}),
+            'services': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['bagni.Service']", 'symmetrical': 'False', 'blank': 'True'}),
+            'site': ('django.db.models.fields.URLField', [], {'max_length': '75', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'tel': ('django.db.models.fields.CharField', [], {'max_length': '125', 'blank': 'True'}),
+            'winter_tel': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'})
+        },
+        u'bagni.image': {
+            'Meta': {'object_name': 'Image'},
+            'bagno': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['bagni.Bagno']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'})
+        },
+        u'bagni.service': {
+            'Meta': {'object_name': 'Service'},
+            'category': ('django.db.models.fields.CharField', [], {'default': "'OT'", 'max_length': '50', 'blank': 'True'}),
+            'free': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'})
+        }
+    }
+
+    complete_apps = ['bagni']
