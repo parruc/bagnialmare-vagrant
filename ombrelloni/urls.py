42559cb5c09c79394982b1b53e31f02ee5c7bf09
Matteo Parrucci <parruc@gmail.com>
2013-08-02 22:46:01 +0200

    activated language switcher

--- ombrelloni/urls.py
+++ ombrelloni/urls.py
@@ -10,6 +10,8 @@ urlpatterns = patterns(
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
