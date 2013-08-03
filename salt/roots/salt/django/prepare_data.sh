04ff2a410d046c7e35ebe56f3f7c21b9391a8272
Matteo Parrucci <parruc@gmail.com>
2013-08-03 14:19:25 +0200

    minor fixes in preparedata

--- salt/roots/salt/django/prepare_data.sh
+++ salt/roots/salt/django/prepare_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-{{ venv.path }}/bin/python manage.py syncdb --noinput --migrate
+{{ venv.path }}/bin/python manage.py syncdb --noinput
+{{ venv.path }}/bin/python manage.py migrate
 {{ venv.path }}/bin/python manage.py loaddata auth.json
-{{ venv.path }}/bin/python manage.py manage_index create
+{{ venv.path }}/bin/python manage.py manage_index -o create
 {{ venv.path }}/bin/python manage.py import_services
 {{ venv.path }}/bin/python manage.py import_bagni
 {{ venv.path }}/bin/python manage.py collectstatic -l --noinput
-{{ venv.path }}/bin/python manage.py makemessages -a
-{{ venv.path }}/bin/python manage.py compilemessages -a
+{{ venv.path }}/bin/python manage.py compilemessages
