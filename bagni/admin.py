5340e0d12a8fccbc4cca4a0b36eebc17deeff061
Matteo Parrucci <parruc@gmail.com>
2013-08-25 09:38:28 +0200

    added model translation

--- bagni/admin.py
+++ bagni/admin.py
@@ -1,6 +1,21 @@
-from django.contrib.gis import admin
+from django.contrib import admin
+from django.contrib.gis.admin import GeoModelAdmin
+from modeltranslation.admin import TranslationAdmin
 import models
 
-admin.site.register(models.Bagno, admin.GeoModelAdmin)
-admin.site.register(models.Service, admin.GeoModelAdmin)
-admin.site.register(models.Image, admin.GeoModelAdmin)
+
+class BagnoAdmin(TranslationAdmin, GeoModelAdmin):
+    pass
+
+
+class ServiceAdmin(TranslationAdmin, GeoModelAdmin):
+    pass
+
+
+class ImageAdmin(TranslationAdmin, GeoModelAdmin):
+    pass
+
+
+admin.site.register(models.Bagno, BagnoAdmin)
+admin.site.register(models.Service, ServiceAdmin)
+admin.site.register(models.Image, ImageAdmin)
