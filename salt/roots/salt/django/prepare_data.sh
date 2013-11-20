e03f98f6bab0e5b1a2e97f944ef12e8d5fe571ad
Matteo Parrucci <parruc@gmail.com>
2013-11-20 11:55:05 +0100

    better prepare data now reindexes at the end

--- salt/roots/salt/django/prepare_data.sh
+++ salt/roots/salt/django/prepare_data.sh
@@ -2,13 +2,12 @@
 
 {% set venv = pillar.venv.venvs[django_name] %}
 
-{{ venv.path }}/bin/python manage.py migrate
-{{ venv.path }}/bin/python manage.py syncdb --noinput
+{{ venv.path }}/bin/python manage.py syncdb --noinput --migrate
 {{ venv.path }}/bin/python manage.py loaddata auth.json
 {{ venv.path }}/bin/python manage.py manage_index -o create
 {{ venv.path }}/bin/python manage.py import_services
 {{ venv.path }}/bin/python manage.py import_bagni
 {{ venv.path }}/bin/python manage.py collectstatic -l --noinput
 {{ venv.path }}/bin/python manage.py compilemessages
-{{ venv.path }}/bin/python manage.py sync_translation_fields --noinput
 {{ venv.path }}/bin/python manage.py update_translation_fields
+{{ venv.path }}/bin/python manage.py manage_index -o rebuild
