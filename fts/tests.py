57811cf1e942c113653f721f37a8c413a7c9085d
flyingfrog <marco.bartolini@gmail.com>
2014-01-03 18:57:20 +0100

    reestabilished functional tests and added one

--- fts/tests.py
+++ fts/tests.py
@@ -10,20 +10,26 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class HomePageTest(LiveServerTestCase):
 
-    #fixtures = ['bagni.json']
-
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.browser = webdriver.Remote(
                 command_executor = "http://127.0.0.1:4444/wd/hub",
                 desired_capabilities = DesiredCapabilities.FIREFOX,
                 )
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         self.browser.quit()
 
     def test_home_page(self):
-        self.browser.get("http://127.0.0.1:8080/it/")
-        h1 = self.browser.find_element_by_id("content-title")
-        self.assertEqual(h1.text, "Homepage")
+        self.browser.get("http://127.0.0.1:8081/it/")
+        self.assertEqual(self.browser.title, "Homepage")
+
+    def test_empty_search(self):
+        self.browser.get("http://127.0.0.1:8081/it/")
+        input = self.browser.find_element_by_id("search_q")        
+        input.send_keys(Keys.RETURN)
+        count = self.browser.find_element_by_class_name("hits-count")
+        self.assertTrue(int(count.text) > 1000)
 
