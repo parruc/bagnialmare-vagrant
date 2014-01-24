a9b01841683e886483e5b0be88bf44cd34c5083e
Matteo Parrucci <parruc@gmail.com>
2014-01-24 23:49:50 +0100

    conditional repo change to avoid doing it inside the 4hm one

--- prepare_development_parruc.sh
+++ prepare_development_parruc.sh
@@ -14,9 +14,9 @@ echo "mounting $GUST_DIR to $HOST_DIR"
 sudo mount -t cifs //192.168.50.5/ombrelloni /home/creepingdeath/4hm -o username=ombrelloni,password=ombrelloni,uid=creepingdeath
 echo "changing git credentials for your user: $GIT_USER"
 cd "$HOST_DIR/django"
-git config --global user.email "parruc@gmail.com"
-git config --global user.name "Matteo Parrucci"
 if [ "$PWD"=="$HOST_DIR/django" ]; then
+    git config --global user.email "parruc@gmail.com"
+    git config --global user.name "Matteo Parrucci"
     git remote rm origin
     git remote add origin https://$GIT_USER@bitbucket.org/flyingfrog/4hm.git
 fi
