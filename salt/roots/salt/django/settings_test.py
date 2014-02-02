d05e098cc39c53bf8398d69299ce752112126f85
Marco Bartolini <marco.bartolini@gmail.com>
2014-02-02 13:33:41 +0100

    exclude debug toolbar from test settings

--- salt/roots/salt/django/settings_test.py
+++ salt/roots/salt/django/settings_test.py
@@ -1,6 +1,16 @@
 #settings file for running tests
 from settings import *
 
+def remove_from_tuple(t, el):
+    if el in t:
+        index = t.index(el)
+        return t[:index] + t[index + 1:]
+    else:
+        return t
+
 {% set db = pillar['pg'].dbs['test_' + django_name] %}
 
 DATABASES['default']['USER'] = '{{db.owner}}'
+
+INSTALLED_APPS = remove_from_tuple(INSTALLED_APPS, 'debug_toolbar')
+MIDDLEWARE_CLASSES = remove_from_tuple(MIDDLEWARE_CLASSES, 'debug_toolbar.middleware.DebugToolbarMiddleware')
