2cb422a2200c5e7dd406681840c217bc7445fab3
Matteo Parrucci <parruc@gmail.com>
2014-02-02 11:40:19 +0100

    perchè reimpostavo il remote?

--- prepare_development_parruc.sh
+++ prepare_development_parruc.sh
@@ -17,8 +17,8 @@ cd "$HOST_DIR/django"
 if [ "$PWD"=="$HOST_DIR/django" ]; then
     git config --global user.email "parruc@gmail.com"
     git config --global user.name "Matteo Parrucci"
-    git remote rm origin
-    git remote add origin https://$GIT_USER@bitbucket.org/flyingfrog/4hm.git
+#    git remote rm origin
+#    git remote add origin https://$GIT_USER@bitbucket.org/flyingfrog/4hm.git
 fi
 
 
