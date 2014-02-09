ed82584deab0ca7a0fe3148c22e96deaac83e38e
Marco Bartolini <marco.bartolini@gmail.com>
2014-02-09 17:16:20 +0100

    changed whoosh debug instance

--- salt/roots/salt/django/settings_test.py
+++ salt/roots/salt/django/settings_test.py
@@ -11,6 +11,7 @@ def remove_from_tuple(t, el):
 {% set db = pillar['pg'].dbs['test_' + django_name] %}
 
 DATABASES['default']['USER'] = '{{db.owner}}'
+WHOOSH_INDEX = os.path.join(os.path.dirname(__file__), 'whoosh_index_test')
 
 INSTALLED_APPS = remove_from_tuple(INSTALLED_APPS, 'debug_toolbar')
 MIDDLEWARE_CLASSES = remove_from_tuple(MIDDLEWARE_CLASSES, 'debug_toolbar.middleware.DebugToolbarMiddleware')
