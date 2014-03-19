48d67fdce234a5110ee4436b266d57c59d89ffdf
Matteo Parrucci <parruc@gmail.com>
2014-03-19 18:20:28 +0100

    dbreset now migrates and reindexes with the right user

--- salt/roots/salt/cronjobs/dbutils/dbreset.sh
+++ salt/roots/salt/cronjobs/dbutils/dbreset.sh
@@ -3,7 +3,9 @@
 {% set db = pillar.pg.dbs[django_name] %}
 {% set venv = pillar.venv.venvs[django_name] %}
 {% set db_user = pillar.users['postgres'] %}
+{% set user = pillar.users[django_name] %}
 
 sudo -u {{ db_user.name }} dropdb {{ db.name }}
 cat ombrelloni/fixtures/dump.sql | sudo -u {{ db_user.name }} psql
-{{ venv.path }}/bin/python manage.py manage_index -o rebuild
+sudo -u {{ user.name }} {{ venv.path }}/bin/python manage.py migrate
+sudo -u {{ user.name }} {{ venv.path }}/bin/python manage.py manage_index -o rebuild
