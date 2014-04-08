be2505488531ce7f3c2386588ee874240634b89d
Matteo Parrucci <parruc@gmail.com>
2014-04-08 19:24:22 +0200

    registration form now sends mail and does not asks anymore a username + removed loaddata

--- salt/roots/salt/django/prepare_data.sh
+++ salt/roots/salt/django/prepare_data.sh
@@ -5,11 +5,6 @@
 
 {{ venv.path }}/bin/python manage.py syncdb --noinput --migrate
 {{ venv.path }}/bin/python manage.py manage_index -o create
-{% if loc %}
-{{ venv.path }}/bin/python manage.py loaddata dump.json
-{% else %}
-echo "NOT IMPORTING DATA OUTSIDE LOCAL BOX TO AVOID OVERRIDE"
-{% endif %}
 {{ venv.path }}/bin/python manage.py collectstatic -l --noinput
 {{ venv.path }}/bin/python manage.py compilemessages
 {{ venv.path }}/bin/python manage.py manage_index -o rebuild
