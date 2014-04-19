02be960df2529e2264add7c6b8b4aeacb7a42a93
Matteo Parrucci <parruc@gmail.com>
2014-04-20 00:44:06 +0200

    added starting dot as wildcard shown in docs

--- salt/roots/salt/django/settings.py
+++ salt/roots/salt/django/settings.py
@@ -89,7 +89,7 @@ COMPRESS_JS_FILTERS = [
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#\wed-hosts
-ALLOWED_HOSTS = ['{{ host.server_name }}', 'www.{{ host.server_name }}', ]
+ALLOWED_HOSTS = ['.{{ host.server_name }}', ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
