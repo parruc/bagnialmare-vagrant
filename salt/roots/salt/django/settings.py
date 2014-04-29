0e2459c8473762d0130ddfc8dd21b4935b44e43c
Matteo Parrucci <parruc@gmail.com>
2014-04-29 13:05:10 +0200

    added formatters to logs

--- salt/roots/salt/django/settings.py
+++ salt/roots/salt/django/settings.py
@@ -254,21 +254,32 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugTrue',
         },
     },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'simple',
         },
         'console':{
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
             'filters': ['require_debug_true'],
+            'formatter': 'simple',
         },
         'file':{
             'level': 'WARNING',
             'class': 'logging.FileHandler',
             'filename': '{{ django.logs_path }}/django.log',
+            'formatter': 'verbose',
         },
     },
     'loggers': {
